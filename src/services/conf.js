9670116f1aeb9e6461e805c73c953dffc091782a
Rohit Kumar <rohit7091407599@gmail.com>
2024-05-15 13:21:02 +0530

    change server url in env and remove from vite config

--- src/services/conf.js
+++ src/services/conf.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  // baseURL: `${import.meta.env.VITE_SERVER_URI}/api/v1`,
-  baseURL: `/api/v1`,
+  baseURL: `${import.meta.env.VITE_SERVER_URI}/api/v1`,
+  // baseURL: `/api/v1`,
   withCredentials: true,
   headers: {
     Accept: "application/json",
